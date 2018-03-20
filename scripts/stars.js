ae565f8276ad62c90dea232b165416f687fec063
Chris <clj@ollure.com>
2018-03-19 20:55:49 -0500

    added crawl text  working on animation

--- scripts/stars.js
+++ scripts/stars.js
@@ -6,8 +6,8 @@ for (let i = 0; i < numOfStars; i++) {
   let star = document.createElement("div");
     star.className = "star";
     var xy = getRandomPosition();
-    star.style.top = xy[0] +'px';
-    star.style.left = xy[1] + 'px';
+      star.style.top = xy[0] +'px';
+      star.style.left = xy[1] + 'px';
     document.body.append(star);
 }
 
@@ -16,7 +16,8 @@ for (let i = 0; i < numOfStars; i++) {
 function getRandomPosition() {
   var y = window.innerWidth;
   var x = window.innerHeight;
-  var randomX = Math.floor(Math.random()*x);
-  var randomY = Math.floor(Math.random()*y);
-  return [randomX,randomY];
+    var randomX = Math.floor(Math.random()*x);
+    var randomY = Math.floor(Math.random()*y);
+    return [randomX,randomY];
 }
+
